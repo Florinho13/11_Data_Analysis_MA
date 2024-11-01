ba3f9a91d26bfcbc7d465a555da31271e951d133
Florian Angelo Christ <130042303+Florinho13@users.noreply.github.com>
2024-11-01 09:41:42 +0100

    Simple Data wrangling of texture data

--- 03_R/01_load_clean_data.R
+++ 03_R/01_load_clean_data.R
@@ -1,10 +1,17 @@
+#00 Information
 #Transformation of texture textfile to Excel
+#Code created by Florian Christ as part of the Master Thesis
+#used R Version. 4.4.1 2024-06-14 ucrt
 
-#import libraries
+#1. import libraries -----------------------------
 library(openxlsx)
 library(readr)
 library(tidyverse)
 
-texture_data <- read.csv("./01_input/Christ_MSc_Texture_all.txt")
 
+#2. transform to .txt to .xlsx ---------------------------------
+texture_data <- read.csv("./01_input/Christ_MSc_Texture_all.txt")
 write.xlsx(texture_data,"./01_input/christ_msc_texture_all.xlsx")
+
+texture_data <- texture_data
+
