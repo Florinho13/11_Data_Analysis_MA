74ff91a48eb420cf3f26f72ca4c5320d12d67fa0
Florian Angelo Christ <130042303+Florinho13@users.noreply.github.com>
2024-11-01 12:18:50 +0100

    Create Texture Triangle Plot 1

--- 03_R/01_load_clean_data.R
+++ 03_R/01_load_clean_data.R
@@ -1,6 +1,6 @@
 #00 Information
 #Transformation of texture textfile to Excel
-#Code created by Florian Christ as part of the Master Thesis
+#Code created by Florian Christ as part of the Master Thesis 01.11.2024
 #used R Version. 4.4.1 2024-06-14 ucrt
 
 #1. import libraries -----------------------------
@@ -11,7 +11,7 @@ library(tidyverse)
 
 #2. import datasets ---------------------------------
 texture_data <- read.csv("./01_input/Christ_MSc_Texture_all.txt")
-write.xlsx(texture_data,"./01_input/christ_msc_texture_all.xlsx")
+#write.xlsx(texture_data,"./01_input/christ_msc_texture_all.xlsx")
 
 #3. clean texture_data
 #Simplify sample name and set column data types
