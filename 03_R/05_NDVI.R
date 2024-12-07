cfd0564bd7773e933f1ece1878486f3acb4f9d39
Florian Angelo Christ <130042303+Florinho13@users.noreply.github.com>
2024-12-07 21:52:19 +0100

    several small changes

--- 03_R/05_NDVI.R
+++ 03_R/05_NDVI.R
@@ -2,7 +2,6 @@
 #Calculation of NDVI per Field on 3 different dates.
 #Code created by Florian Christ as part of the Master Thesis 01.11.2024
 #used R Version. 4.4.1 2024-06-14 ucrt
-install.packages("terra")
 
 #1. import libraries and functions -----------------------------
 library(openxlsx)
@@ -58,7 +57,7 @@ S2_dataframe <- data.frame(band4_files,band8_files)
 S2_dataframe <- S2_dataframe %>% 
   mutate(NDVI_name = paste("ndvi_",substr(band4_files,1,22),sep = "")) #add a name for the NDVI rasters.
 
-#ndvi_list <- list() #create an empty list to store the NDVI rasters within.
+ndvi_list <- list() #create an empty list to store the NDVI rasters within.
 
 #calculate NDVI for every date and tile used
 for (i in 1:nrow(S2_dataframe)){
@@ -85,13 +84,51 @@ for (name in names(ndvi_list)){
   
 }
 
-field_1_1_1 <- st_transform(field_1_1_1,crs = ndvi_list[["ndvi_T32TMT_20240203T103231"]])
 
-ndvi_crs <- crs(ndvi_list[["ndvi_T32TMT_20240203T103231"]])
+#4. Prepare NDVI per field calculations and visualisations
+field_polygons <- ls(pattern = "Field_Code")
 
-field_1_1_1_extent <- ext(field_1_1_1)
-field_1_1_1 <- st_transform(Field_Code_1_1_1, crs = 32632)
-st_crs(field_1_1_1)
+
+#transform to EPSG32632
+for(name in field_polygons){
+  polygon_data <- get(name)
+  
+  polygon_trans_to_32632(polygon_data,name)
+  }
+
+
+
+
+#create field extents
+#list of fields with transformed ellipsoid
+transformed_fields <- ls(pattern = "^field_[0-9]_[0-9]_[0-9]$")
+
+list_of_extents <- list() #prepare list containing field extents
+
+for (name in transformed_fields){
+  #create name for field extents
+  extent_name <- paste(name,"extent",sep = "_")
+  
+  #get the field corresponding sf
+  field <- get(name)
+  
+  #create the extent shapefile.
+  extent_field <- ext(field)
+  
+  #create list of field extenr 
+  list_of_extents <- append(list_of_extents,extent_name)
+  
+  #assign extent name to the field.
+  assign(extent_name, extent_field)
+}
+
+
+# Combine all extent polygons into one SpatVector
+all_extents <- do.call(rbind, list_of_extents)
+
+# Plot all extents
+plot(all_extents, col = NA, border = "red", main = "Field Extents")
+
 ndvi_cropped <- crop(ndvi_list[["ndvi_T32TMT_20240203T103231"]],field_1_1_1_extent)
 
 plot(ndvi_list[["ndvi_T32TMT_20240203T103231"]],col = terrain.colors(50),ext = field_1_1_1_extent)
