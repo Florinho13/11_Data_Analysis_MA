adb64d59593e118fccd46bc0d8e50a839db24a40
Florian Angelo Christ <130042303+Florinho13@users.noreply.github.com>
2025-01-21 18:45:40 +0100

    save KAK files

--- 03_R/01_load_clean_data.R
+++ 03_R/01_load_clean_data.R
@@ -7,6 +7,7 @@
 library(openxlsx)
 library(readr)
 library(tidyverse)
+library(janitor)
 
 
 #2. import datasets ---------------------------------
@@ -30,6 +31,17 @@ microbial_N <- read.xlsx("./01_input/Data_microbial_biomass_BT_Janina_Hämmerli.
 plant_height <- read.xlsx("./01_input/Plant_Health.xlsx",sheet="höhe")
 #Chlorophyll content
 plant_chlorophyll <- read.xlsx("./01_input/Plant_Health.xlsx",sheet = "chlorophyll")
+
+
+#KAK
+#results KAK Ca
+KAK_Ca <- read.xlsx("./01_input/2024_09_20_KAK_all_Florian.xlsx",sheet="KAK_results_Ca")
+#results KAK NaMgAlKMn
+KAK_NaMgAlKMn <- read.xlsx("./01_input/2024_09_20_KAK_all_Florian.xlsx",sheet="KAK_results_NaMgAlKMn")
+#blanks
+KAK_blanks <- read.xlsx("./01_input/2024_09_20_KAK_all_Florian.xlsx",sheet="blanks")
+#element properties
+KAK_elements <- read.xlsx("./01_input/2024_09_20_KAK_all_Florian.xlsx",sheet="elements")
 
 
 #3. clean texture_data#####
@@ -97,6 +109,7 @@ plant_chlorophyll_clean <- plant_chlorophyll %>%
 
 saveRDS(plant_chlorophyll_clean,"./01_input/plant_chlorophyll_data_clean.rds")
 
+
 plant_chlorophyll_clean_long <- plant_chlorophyll_clean %>%
   pivot_longer(
     cols = c(chlorophyll_mid, chlorophyll_front),
@@ -106,3 +119,67 @@ plant_chlorophyll_clean_long <- plant_chlorophyll_clean %>%
 
 saveRDS(plant_chlorophyll_clean_long,"./01_input/plant_chlorophyll_data_clean_long.rds")
 
+
+
+#7. clean KAK data####
+#KAK Ca
+KAK_Ca_clean <- KAK_Ca %>% 
+  select(1:12) %>% 
+  row_to_names(row_number = 1) %>% 
+  slice(-n())
+
+#KAK NaMgAlKMn
+KAK_NaMgAlKMn_clean <- KAK_NaMgAlKMn %>% 
+  row_to_names(row_number = 1) %>% 
+  mutate_at(vars(4:15),as.numeric)
+
+#calculate the CEC as well as the cation concentration per kg for each measured element
+vec_elements <- KAK_elements$element[2:6]
+
+#generate dataframe to store calculated data
+KAK_NaMgAlKMn_clean_plus <- KAK_NaMgAlKMn_clean
+
+
+#calculate columns containing the Element content in mg/kg as well as the cation exchange capacity
+#a for loop is used for this purpose iterating through all the elements
+for (elem in vec_elements){
+  # store elements ammount of valence electrons
+  e_Ladung <- KAK_elements$Ladung[KAK_elements$element == elem]
+  
+  # store elements molar mass
+  molare_masse <- KAK_elements$`MM_mg/mM`[KAK_elements$element == elem]
+  
+  # store average blank value for element
+  blank_average <- KAK_blanks$bl_average[KAK_blanks$element == elem]
+  
+  #new column name for cation weight per kg soil
+  coln_mg_kg <- paste(elem,"conc_mg_kg",sep = "_")
+  
+  #new column name for CEC
+  coln_CEC <- paste(elem,"aus_kation_mmol_kg", sep = "_")
+  
+  #object used to select column
+  colselecter <- paste(elem,"Conc_ug",sep = "_")
+  
+  #store column containing all measured concentrations in the soil samples
+  conc_column_ug_l <- KAK_NaMgAlKMn_clean %>% 
+    select(starts_with(colselecter)) %>% 
+    pull()
+  
+  #calculate column containing cation weight per kilo soil
+  KAK_NaMgAlKMn_clean_plus <- KAK_NaMgAlKMn_clean_plus %>% 
+    mutate(!!coln_mg_kg := ((((conc_column_ug_l*20)-(blank_average*20))*(100/1000)/(KAK_NaMgAlKMn_clean$einwaage_g/1000))/1000))
+  #calculate column containing the cation exchange capacity
+  KAK_NaMgAlKMn_clean_plus <- KAK_NaMgAlKMn_clean_plus %>% 
+    mutate(!!coln_CEC := (!!sym(coln_mg_kg) /molare_masse)*e_Ladung)
+  
+}
+
+KAK_NaMgAlKMn_clean_plus_ordered_selected <- KAK_NaMgAlKMn_clean_plus_ordered %>% 
+  select(2,3,6:25)
+
+KAK_combined <- KAK_Ca_clean %>% 
+  left_join(KAK_NaMgAlKMn_clean_plus_ordered_selected,by = c("sample" = "sample", "replicate" = "replicate"),keep = FALSE)
+
+write.xlsx(KAK_combined,"./02_output/KAK_all_excel_2024_09_20.xlsx")
+saveRDS(KAK_combined,"./01_input/KAK_all_2024_09_20.rds")
