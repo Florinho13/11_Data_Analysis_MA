11ef6b1b75df3545734b96feae7fa32a21868fce
Florian Angelo Christ <130042303+Florinho13@users.noreply.github.com>
2025-08-15 16:38:27 +0200

    updated soil health index

--- .Rproj.user/DA4BCAA9/sources/session-eca806b4/35FBEB68-contents
+++ .Rproj.user/DA4BCAA9/sources/session-eca806b4/35FBEB68-contents
@@ -19,7 +19,8 @@ source("./03_R/00_functions.R")
 
 #import datasets
 soil_combined <- readRDS("./01_input/soil_data_combined.rds")
-write.xlsx(soil_combined,"./02_output/soil_data_combined.xlsx")
+soil_risk <- readRDS("./02_output/11_ppp_comparison/ppp_all_cleaned_long_meta_rq.rds")
+#write.xlsx(soil_combined,"./02_output/soil_data_combined.xlsx")
 
 #1. create SHI dataframe ####
 #first preparation step
@@ -53,9 +54,33 @@ for(i in variables){
 write.xlsx(shi_dataset_basic_ph,"./02_output/08_SHI/shi_basic_data.xlsx")
 
 
+#1.2 Prepare RQ data for SHI
+soil_risk_cum_plot <- soil_risk %>% 
+  group_by(sample) %>% 
+  summarise(cumulative_RQ = sum(risk_quotient,na.rm = TRUE),
+            .groups = "drop") %>%
+  rename("sample_name" = "sample")
+
+
+soil_risk_cum_plot_classified <- soil_risk_cum_plot %>% 
+  mutate(rq_classified = 
+           case_when(
+    cumulative_RQ < 0.01 ~ 1,
+    cumulative_RQ >= 0.01 & cumulative_RQ < 0.1 ~ 0.7,
+    cumulative_RQ >= 0.1 & cumulative_RQ < 1 ~ 0.3,
+    cumulative_RQ > 1 ~0,
+    TRUE ~ cumulative_RQ)
+    )
+
 #2. calculate SHI scores####
 scored_soil_data <- score_shi(shi_dataset_basic_ph)
 
+scored_soil_data_rq <- scored_soil_data %>% 
+  left_join(soil_risk_cum_plot_classified, by = "sample_name") %>% 
+  select(-cumulative_RQ)
+
+scored_soil_data_rq_total <- scored_soil_data_rq %>% 
+  mutate(shi_total = rowSums(across(5:12),na.rm = TRUE))
 
 #calculate mean per field
 shi_mean <- scored_soil_data %>% 
@@ -74,7 +99,21 @@ shi_mean_clean <- shi_mean %>%
 shi_mean_total <- shi_mean_clean %>% 
   mutate(shi_total = rowSums(across(4:10), na.rm = TRUE))
 
+
+shi_mean_rq_total <- scored_soil_data_rq_total %>% 
+  group_by(location,farming_system) %>% 
+  summarise(across(
+    .cols = c("pH", "microbial_c", "Mg_aus_kation_mmol_kg", 
+              "Na_aus_kation_mmol_kg", "C%_normal", "C/N_normal", 
+              "mean_BD_g_cm3", "rq_classified", "shi_total"),
+    .fns = mean,
+    .names = "{.col}"
+  ),
+  .groups = "drop") %>% 
+  mutate("Field ID" = paste(location,farming_system,sep = "_"),.before = 1)
+
 write.xlsx(shi_mean_total,"./02_output/08_SHI/shi_mean_total.xlsx")
+write.xlsx(shi_mean_rq_total,"./02_output/08_SHI/shi_mean_total_rq.xlsx")
 
 #prepare for ploting
 #rearange data
@@ -90,14 +129,44 @@ shi_cleeveland <- shi_cleeveland %>%
   rename("Regenerative" = 3, "Conventional" = 4)
 
 
+shi_mean_rq_total_prep <- shi_mean_rq_total %>%
+  rename("RQ score" = "rq_classified") %>% 
+  pivot_longer(cols = 4:12, names_to = "variable", values_to = "SHI score")
+
+shi_mean_rq_total_prep <- clean_soil_names(shi_mean_rq_total_prep,variable)
+
+shi_cleeveland_rq <- shi_mean_rq_total_prep %>% 
+  select(-`Field ID`) %>% 
+  pivot_wider(names_from = farming_system, values_from = `SHI score`)
+shi_cleeveland_rq <- shi_cleeveland_rq %>% 
+  rename("Regenerative" = 3, "Conventional" = 4)
+
+shi_cleeveland_rq_plot <- shi_cleeveland_rq %>% 
+  filter(variable != "shi_total")
+
+
+# 1) One label per location
+loc_labels <- shi_cleeveland_rq_plot %>%
+  group_by(location) %>%
+  summarise(
+    r_better = sum(Regenerative > Conventional, na.rm = TRUE),
+    c_better = sum(Conventional > Regenerative, na.rm = TRUE),
+    n_vars   = sum(!is.na(Regenerative) & !is.na(Conventional)),
+    .groups = "drop"
+  ) %>%
+  mutate(label = sprintf("R vs. C: %d-%d", r_better, c_better)) %>% 
+  mutate(label = if_else(label=="Reg vs. Conv: 0-0","no comp.", label))
+
+
+
 #3. generat cleeveland dot plot
 location_labels <- c(
-  "1" = "Freudwil",
-  "2" = "Niederhasli",
-  "3" = "Läufelfingen",
-  "4" = "Heimenhausen",
-  "5" = "Heimiswil",
-  "6" = "Ueberstorf"
+  "1" = "Freudwil (R = 5.35)",
+  "2" = "Niederhasli (R = 4.23, C = 3.45)",
+  "3" = "Läufelfingen (R = 6.27, C = 5.72)",
+  "4" = "Heimenhaus. (R = 5.44, C = 3.81)",
+  "5" = "Heimiswil (R = 4.84, C = 4.00)",
+  "6" = "Ueberstorf (R = 4.83, C = 3.62)"
 )
 
 
@@ -124,6 +193,48 @@ shi_plot <- ggplot(shi_cleeveland) +
     y = "Soil Health Parameter"
   )
 shi_plot
+
+
 ggsave("./02_output/08_shi/shi_cleeveland_plot.png",plot = shi_plot,
        width = 19, height=27, units = "cm", dpi = 300)
 
+
+
+#final rq plot
+
+
+
+shi_plot_rq <- ggplot(shi_cleeveland_rq_plot) +
+  geom_segment(aes(x = Regenerative, xend = Conventional,
+                   y = variable, yend = variable), color = "#2C3E50",alpha=0.7) +
+  geom_point(aes(x = Regenerative, y = variable), color = "#66c2a5", size = 3) +
+  geom_point(aes(x = Conventional, y = variable), color = "#fc8d62", size = 3) +
+  facet_wrap(~location, scales = "free_x",nrow = 3,
+             labeller = as_labeller(location_labels)) +
+  scale_x_continuous(limits = c(0,1))+
+  
+  # bottom-left annotation per facet
+  geom_text(data = loc_labels,
+            aes(x = 0.01, y = -Inf, label = label),
+            hjust = 0, vjust = -0.6, size = 3.5,
+            inherit.aes = FALSE) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 15,face = "bold",hjust = 0.5),
+    axis.title.x = element_text(size=13),
+    axis.title.y = element_text(size=13),
+    axis.text.x = element_text(size = 11),
+    axis.text.y = element_text(size =11),
+    strip.text = element_text(size = 13),
+    panel.border = element_rect(color = "grey60", fill = NA, linewidth = 0.5)
+  )+
+  labs(
+    title = "Cleveland Dot Plot of SHI Scores by Variable and Location",
+    x = "Score",
+    y = "Soil Health Parameter"
+  )
+shi_plot_rq
+
+
+ggsave("./02_output/08_shi/shi_cleeveland_plot_MA.png",plot = shi_plot_rq,
+       width = 19, height=27, units = "cm", dpi = 300)
--- 03_R/16_ppp_comparison_sprint.R
+++ 03_R/16_ppp_comparison_sprint.R
@@ -70,8 +70,7 @@ comparison_boxplot_number_of_detections <- ggplot(data = sprint_thesis_combined_
        aes(x = dataset,y = n_detected, fill = farming_system))+
   geom_boxplot() +
   stat_boxplot(geom ='errorbar',position = position_dodge(width = 0.9)) +
-  ggtitle("Comparison Boxplot of Different Datasets and Farming Systems
-          Number of Detected Substances")+
+  ggtitle("Total Number of Detected Substances on Investigated Plots from SPRINT & Thesis")+
   stat_summary(
     fun = mean,
     geom = "crossbar",
@@ -88,7 +87,7 @@ comparison_boxplot_number_of_detections <- ggplot(data = sprint_thesis_combined_
     "CH" = "CH, Orchards*"
   )) +
   labs(x = "Datasets",
-       y = "Number of Compounds",
+       y = "Number of Substances",
        fill = "Farming System")+
   coord_flip()+
   theme_minimal()+
@@ -97,7 +96,7 @@ comparison_boxplot_number_of_detections <- ggplot(data = sprint_thesis_combined_
 comparison_boxplot_number_of_detections
 
 
-#get total concentrations of both datasets
+#1.3 get total concentrations of both datasets####
 sprint_ppp_soil_long_meta_tot_conc_plot <- sprint_ppp_soil_long_meta %>% 
   group_by(sample, farming_system) %>% 
   summarise(total_concentrations = sum(concentrations_ng_g,na.rm = TRUE), .groups = "drop") %>% 
@@ -114,12 +113,16 @@ thesis_ppp_soil_long_meta_tot_conc_plot <- ppp_all_cleaned_long_meta_rq %>%
 sprint_thesis_combined_tot_conc_plot <- thesis_ppp_soil_long_meta_tot_conc_plot %>% 
   bind_rows(sprint_ppp_soil_long_meta_tot_conc_plot)
 
+saveRDS(sprint_thesis_combined_tot_conc_plot,"./02_output/11_ppp_comparison/sprint_thesis_combined_tot_conc_plot")
+write.xlsx(sprint_thesis_combined_tot_conc_plot,"./02_output/11_ppp_comparison/sprint_thesis_combined_tot_conc_plot.xlsx")
+
+
+
 comparison_boxplot_tot_concentrations <- ggplot(data = sprint_thesis_combined_tot_conc_plot,
                                                   aes(x = dataset,y = total_concentrations, fill = farming_system))+
   geom_boxplot() +
   stat_boxplot(geom ='errorbar',position = position_dodge(width = 0.9)) +
-  ggtitle("Comparison Boxplot of Different Datasets and Farming Systems
-          Total Concentration of Detected Substances")+
+  ggtitle("Total Concentration of Detected Substances on Investigated Plots from SPRINT & Thesis")+
   stat_summary(
     fun = mean,
     geom = "crossbar",
@@ -145,5 +148,90 @@ comparison_boxplot_tot_concentrations <- ggplot(data = sprint_thesis_combined_to
 
 comparison_boxplot_tot_concentrations
 
+#1.4 get cumulative Risk Quotient of both datasets #######
+#multiplicate pnec (mg/kg) with 1000 in order to come to (ng/g)
+pnec_soil_data_clean <- pnec_soil_data %>% 
+  mutate(pnec_soil_ng_g = as.numeric(`PNEC_soil.(mg/kg)`)*1000) %>% 
+  select(-`PNEC_soil.(mg/kg)`)
+
+#prepare rq for SPRINT data
+sprint_ppp_soil_all_cleaned_long_meta_pnec <- sprint_ppp_soil_long_meta %>% 
+  left_join(pnec_soil_data_clean,by = join_by(ppp_compound == PPP_compound_clean))
+
+sprint_ppp_soil_all_cleaned_long_meta_pnec_rq <- sprint_ppp_soil_all_cleaned_long_meta_pnec %>% 
+  mutate(risk_quotient = concentrations_ng_g/pnec_soil_ng_g)
+
+sprint_ppp_soil_long_meta_rq_plot <- sprint_ppp_soil_all_cleaned_long_meta_pnec_rq %>% 
+  group_by(sample, farming_system) %>% 
+  summarise(cumulative_rq = sum(risk_quotient,na.rm = TRUE), .groups = "drop") %>% 
+  mutate(farming_system = str_replace(farming_system, "Conventional","2"),
+         farming_system = str_replace(farming_system, "Organic","3")) %>% 
+  mutate(dataset = substr(sample,1,2)) %>% 
+  filter(dataset %in% c("CZ","CH"))
+
+thesis_ppp_soil_long_meta_rq_plot <- ppp_all_cleaned_long_meta_rq %>% 
+  group_by(sample,farming_system) %>% 
+  summarise(cumulative_rq = sum(risk_quotient,na.rm = TRUE), .groups = "drop") %>% 
+  mutate(dataset = "Thesis")
+
+sprint_thesis_combined_rq_plot <- thesis_ppp_soil_long_meta_rq_plot %>% 
+  bind_rows(sprint_ppp_soil_long_meta_rq_plot)
+
+comparison_boxplot_cumulative_rq <- ggplot(data = sprint_thesis_combined_rq_plot,
+                                                  aes(x = dataset,y = cumulative_rq, fill = farming_system))+
+  geom_boxplot() +
+  stat_boxplot(geom ='errorbar',position = position_dodge(width = 0.9)) +
+  ggtitle("Cumulative Risk Quotient on Investigated Plots from SPRINT & Thesis")+
+  stat_summary(
+    fun = mean,
+    geom = "crossbar",
+    linetype = "dashed",
+    width = 0.5,        # diamond shape
+    color = "red",
+    fill = "white"
+  )+
+  scale_fill_manual(values = fs_colour_plus, 
+                    labels = c("Regenerative", "Conventional", "Organic"))+
+  scale_x_discrete(labels = c(
+    "Thesis" = "Thesis, Oilseed Rape",
+    "CZ" = "CZ, Oil Plants*",
+    "CH" = "CH, Orchards*"
+  )) +
+  scale_y_continuous(trans = "log10")+
+  labs(x = "Datasets",
+       y = "Cumulative Risk Quotient",
+       fill = "Farming System")+
+  coord_flip()+
+  theme_minimal()+
+  theme(legend.position = "bottom")
+
+comparison_boxplot_cumulative_rq
 
 
+options(scipen = 999)
+combined_comparison_plot <- comparison_boxplot_number_of_detections +  
+  #axis.text.x  = element_blank()) 
+  comparison_boxplot_tot_concentrations +
+  #axis.text.x  = element_blank()) 
+  comparison_boxplot_cumulative_rq +
+  plot_layout(ncol = 1, 
+              heights = c(1, 1, 1), 
+              guides ="collect") +
+  plot_annotation(tag_levels = 'a') & 
+  theme(legend.position = "bottom",
+        plot.title = element_blank())
+
+combined_comparison_plot
+
+
+
+ggsave(
+  "./02_output/11_ppp_comparison/comparison_boxplots_sprint_thesis.png",   # or .jpg
+  combined_comparison_plot,
+  width = 19,               # cm (slightly smaller than full A4 to allow margins)
+  height = 27,              # cm
+  units = "cm",
+  dpi = 300                 # 300 dpi = print quality
+)
+
+combined_plot
